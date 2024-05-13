78f126518b8fb395bf56bac8894abe614d5f42ae
ChairLeg39 <ajlee39@gmail.com>
2024-05-13 15:46:16 -0500

    Finished building schema.sql

--- EmployeeSQL/schema-lee.sql
+++ EmployeeSQL/schema-lee.sql
@@ -1,96 +1,80 @@
--- Create tables and import data
--- Drop table if exists
+-- Drop tables if they exist (order of dropping respects foreign key dependencies)
+DROP TABLE IF EXISTS Salaries;
+DROP TABLE IF EXISTS Dept_Manager;
+DROP TABLE IF EXISTS Dept_Emp;
+DROP TABLE IF EXISTS Employees;
+DROP TABLE IF EXISTS Titles;
 DROP TABLE IF EXISTS Department;
 
--- Create new table
+-- Create Department table
 CREATE TABLE Department (
-	dept_no VARCHAR NOT NULL,
-	dept_name VARCHAR NOT NULL,
-	PRIMARY KEY (dept_no)
+    dept_no VARCHAR NOT NULL,
+    dept_name VARCHAR NOT NULL,
+    PRIMARY KEY (dept_no)
 );
 
--- Verify successful data import
+-- Verify Department table
 SELECT * FROM Department;
 
 
-
--- Drop table if exists
-DROP TABLE IF EXISTS Dept_Emp;
-
--- Create new table
-CREATE TABLE Dept_Emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES Department (dept_no)
+-- Create Titles table
+CREATE TABLE Titles (
+    title_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    PRIMARY KEY (title_id)
 );
 
--- Verify successful data import
-SELECT * FROM Dept_Emp;
+-- Verify Titles table
+SELECT * FROM Titles;
 
 
-
--- Drop table if exists
-DROP TABLE IF EXISTS Dept_Manager;
-
--- Create new table
-CREATE TABLE Dept_Manager (
-	dept_no VARCHAR NOT NULL,
-	emp_no VARCHAR NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES Department (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
-);
-
--- Verify successful data import
-SELECT * FROM Dept_Manager;
-
-
-
--- Drop table if exists
-DROP TABLE IF EXISTS Employees;
-
--- Create new table
+-- Create Employees table
 CREATE TABLE Employees (
-	emp_no INT NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES Titles (title_id),
-	PRIMARY KEY (emp_no)
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id),
+    PRIMARY KEY (emp_no)
 );
 
--- Verify successful data import
+-- Verify Employees table
 SELECT * FROM Employees;
 
 
+-- Create Dept_Emp table
+CREATE TABLE Dept_Emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES Department(dept_no)
+);
 
--- Drop table if exists
-DROP TABLE IF EXISTS Salaries;
+-- Verify Dept_Emp table
+SELECT * FROM Dept_Emp;
 
--- Create new table
+
+-- Create Dept_Manager table
+CREATE TABLE Dept_Manager (
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES Department(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+-- Verify Dept_Manager table
+SELECT * FROM Dept_Manager;
+
+
+-- Create Salaries table
 CREATE TABLE Salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
--- Verify successful data import
+-- Verify Salaries table
 SELECT * FROM Salaries;
-
-
-
--- Drop table if exists
-DROP TABLE IF EXISTS Titles;
-
--- Create new table
-CREATE TABLE Titles (
-	title_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
-	PRIMARY KEY (title_id)
-);
-
--- Verify successful data import
-SELECT * FROM Titles;
